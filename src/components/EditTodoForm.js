37b78f24d9eb9eabb1c8446f1499dd1a93d156a7
devasmi <impokhrelasmita5@gmail.com>
2023-05-08 12:17:10 -0400

    edit and delete button to work

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,18 @@
+import React,{useState} from "react";
+
+export const EditodoForm = ({editTodo,task}) => {
+  const [value, setValue]= useState(task.task)
+  const handleSubmit = e =>{
+    e.preventDefault();
+    editTodo(value);
+    setValue("");
+  }
+
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input type="text" className="todo-input" value={value} placeholder="Update task" onChange={(e)=>setValue(e.target.value)}/>
+      <button type='submit'className="todo-btn">Update Task</button>
+    </form>
+  );
+};
+
